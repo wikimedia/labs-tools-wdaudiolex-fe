4a49b42fc633d177698bfce1373ec968bc4e9373
osongrepo <osongloic4@gmail.com>
2024-07-27 08:07:28 -0700

    latest

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,20 +1,19 @@
-import React from 'react';  
+import React from "react";
 
-interface ButtonProps {  
-  label: string;  
-  onClick: () => void;  
-}  
+interface ButtonProps {
+  label: string;
+  onClick: () => void;
+}
 
-const Button: React.FC<ButtonProps> = ({ label, onClick }) => {  
-  return (  
-    <button  
-      className=" hover:text-white hover:bg-black bg-white animate-bounce text-black  font-bold py-2 px-4 rounded-2xl  transition duration-200"  
-      onClick={onClick}  
-    >  
-      {label}  
-    </button>  
-  );  
-};  
+const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
+  return (
+    <button
+      className=" hover:text-white hover:bg-black bg-white animate-bounce text-black  font-bold py-2 px-4 rounded-2xl  transition duration-300"
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
+};
 
 export default Button;
-
