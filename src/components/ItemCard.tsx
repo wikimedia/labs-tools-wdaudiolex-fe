5401126bcb7b64ddfd0c0b46cd95a86cab27b916
Md Christien <mdchristien12@gmail.com>
2024-07-27 17:45:25 +0100

    refactor the search input

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -105,7 +105,7 @@ const ItemCard = ({
                 </button>
                 <button
                   type="button"
-                  className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-wikimedia-wikiblue text-white hover:bg-blue-700 focus:outline-none focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none"
+                  className="py-1.5 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-wikimedia-wikiblue text-white hover:bg-wikimedia-darkblue focus:outline-none focus:bg-wikimedia-wikiblue disabled:opacity-50 disabled:pointer-events-none"
                 >
                   Save changes
                 </button>
