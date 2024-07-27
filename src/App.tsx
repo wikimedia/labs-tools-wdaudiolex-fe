284a48f043b78f8f35fb387b100d76210cac18e3
unknown <ntuiraoul1>
2024-07-27 10:07:59 -0700

    The complete work

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./layout/Header";
-
+// import dropdown from "..src/components/Dropdown";
+import Dropdown from "./components/dropdown";
 const App = () => {
   return (
     <>
     <Header />
+    <Dropdown />
     </>
   );
 };
--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import Navbar from "../components/Navbar";
+import Dropdown from "../components/dropdown";
 import { Outlet } from "react-router-dom";
+// import '../components/drodown.css';
+import { language } from "../Utils/data";
 
 const Layout = () => {
+  
   return (
     <>
-      <Navbar />
-      <Outlet />
+    <Dropdown placeholder='English' options={language} />
+      {/* <Navbar /> */}
+      {/* <Outlet /> */}
     </>
   );
 };
