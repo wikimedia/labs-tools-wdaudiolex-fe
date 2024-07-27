3685fd1587bf57db5c9a63825d38e10b61cd127f
ndaleghnoelalummbah <ndaleghnoelalum@gmail.com>
2024-07-27 15:48:15 +0100

    merge: resolve merge conflicts

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 import SearchInput from "../components/SearchInput";
+import Pagination from "../components/Pagination/Pagination";
 
 const Layout: React.FC = () => {
+      const handlePageChange = ({ selected }: { selected: number }) => {
+        console.log("hi");
+      };
   return (
     <>
       <Navbar />
       <Outlet />
+      <Pagination pageCount={24} onPageChange={handlePageChange} />
     </>
   );
 };
