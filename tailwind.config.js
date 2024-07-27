7d971f542b621757aff431f059efe1626aefb151
NkwiCyril <akinimbomnkwi@gmail.com>
2024-07-27 06:53:36 +0100

    added preline dependency

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    'node_modules/preline/dist/*.js',
   ],
   theme: {
     extend: {
@@ -30,5 +31,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('preline/plugin'),
+  ],
 }
