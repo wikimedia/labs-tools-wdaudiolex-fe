1d1fb25d51fde802ebaba8406120538c252a0804
NkwiCyril <akinimbomnkwi@gmail.com>
2024-07-27 18:24:11 +0100

    modifying the layout of the navbar

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -8,18 +8,18 @@ function Navbar() {
     alert("welcome to wikimedia ");
   };
   return (
-    <div className=" w-full bg-slate-200 flex justify-between items-center px-12">
+    <div className=" w-full h-fit bg-slate-200 flex justify-between items-center px-12">
       <div className="text-2xl text-black flex w-30  ">
-        <div className="bg-white h-fit p-4 mt-4 ">
-          <FaHome />{" "}
+        <div className="bg-white h-fit p-4 mt-4  hover:text-wikimedia-wikiblue">
+         <a href="#"><FaHome /></a> 
         </div>
-        <p className="pl-10 pt-7 font-thin">About</p>
+        <a href="#"><p className="pl-10 pt-7 font-thin hover:text-wikimedia-wikiblue">About</p></a>
       </div>
 
       <div className="mt-5">
-        <div className="mt-5 flex gap-10">
+        <div className="mt-5 pb-3 flex gap-10 ">
           <SearchInput onSearch={''} />
-          <Button label="Login" onClick={clicked} />
+         <div className="animate-pulse"><Button  label="Login" onClick={clicked} /></div> 
         </div>
       </div>
     </div>
