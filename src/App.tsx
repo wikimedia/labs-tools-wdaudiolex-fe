b62677038cd5fc4788255bd59c268fcf094e89fe
ndaleghnoelalummbah <ndaleghnoelalum@gmail.com>
2024-07-27 13:49:41 +0100

    feat: design the pagination component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Header from "./layout/Header";
+import Pagination from "./components/Pagination/Pagination";
 
 const App = () => {
+    const handlePageChange = ({ selected }: { selected: number }) => {
+      // dispatch(updateListingQueryParams({ page: selected + 1 }));
+      // dispatch(getListingsThunk());
+      console.log('hi');
+      
+    };
   return (
     <>
-    <Header />
+      <Header />
+      <Pagination
+        pageCount={24}
+        onPageChange={handlePageChange}
+      />
     </>
   );
 };
