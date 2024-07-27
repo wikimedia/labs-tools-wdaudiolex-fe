75bf93d82168577ff049d5f271dcf6d4d1d8b0d0
NkwiCyril <akinimbomnkwi@gmail.com>
2024-07-27 15:57:38 +0100

    added button

--- src/pages/Layout.tsx
+++ src/pages/Layout.tsx
@@ -2,12 +2,17 @@ import React from "react";
 import Navbar from "../components/Navbar";
 import { Outlet } from "react-router-dom";
 import SearchInput from "../components/SearchInput";
+import Footer from "../components/Footer";
+import Button from "../components/button";
 
 const Layout: React.FC = () => {
   return (
     <>
       <Navbar />
       <Outlet />
+      <SearchInput onSearch={''}/>
+      <Footer />
+      <Button label="Login" onClick={()=>{}} />
     </>
   );
 };
