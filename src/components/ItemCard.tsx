3b923ffa0ace7c3be52060da083760119ee0325c
Md Christien <mdchristien12@gmail.com>
2024-07-27 17:09:20 +0100

    build the Modal component

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -74,7 +74,7 @@ const ItemCard = ({
           id="hs-vertically-centered-modal"
           className="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
           role="dialog"
-          tabindex="-1"
+          tabIndex={-1}
           aria-labelledby="hs-vertically-centered-modal-label"
         >
           <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)] flex items-center">
@@ -84,31 +84,8 @@ const ItemCard = ({
                   id="hs-vertically-centered-modal-label"
                   className="font-bold text-gray-800 dark:text-white"
                 >
-                  Modal title
+                  Edit Item
                 </h3>
-                <button
-                  type="button"
-                  className="size-8 inline-flex justify-center items-center gap-x-2 rounded-full border border-transparent bg-gray-100 text-gray-800 hover:bg-gray-200 focus:outline-none focus:bg-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-700 dark:hover:bg-neutral-600 dark:text-neutral-400 dark:focus:bg-neutral-600"
-                  aria-label="Close"
-                  data-hs-overlay="#hs-vertically-centered-modal"
-                >
-                  <span className="sr-only">Close</span>
-                  <svg
-                    className="shrink-0 size-4"
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="24"
-                    height="24"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                  >
-                    <path d="M18 6 6 18"></path>
-                    <path d="m6 6 12 12"></path>
-                  </svg>
-                </button>
               </div>
               <div className="p-4 overflow-y-auto">
                 <p className="text-gray-800 dark:text-neutral-400">
