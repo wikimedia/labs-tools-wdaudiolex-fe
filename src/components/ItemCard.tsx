6c1fff7b6aa61856df834d66470fc0c4d2a3436b
ndaleghnoelalummbah <ndaleghnoelalum@gmail.com>
2024-08-03 12:21:11 +0100

    chore: modify button component to be reusable

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -1,10 +1,9 @@
 import { PencilIcon } from "@heroicons/react/24/outline";
-import React from "react";
+import React, { useState } from "react";
 import SearchInput from "./SearchInput";
 import EditSearch from "./EditSearch";
 import { Item } from "../utils/types";
-
-
+import Button from "./button";
 
 const ItemCard = ({
   id,
@@ -15,9 +14,19 @@ const ItemCard = ({
   foundedDate,
 }: Item) => {
   const collapseId = `hs-collapse-${id}`;
+  const [seeMore, setSeeMore] = useState(false);
+  const [showModal, setShowModal] = useState(true);
+  const btnText = seeMore ? "See Less" : "See More ";
+
+  const handleMoreContent = () => {
+    setSeeMore(!seeMore);
+  };
+  const handleEdit = () => {
+    setShowModal(!showModal);
+  };
 
   return (
-    <div className="container m-auto px-3 mb-10">
+    <div className="px-3 mb-10">
       <div className="flex flex-col gap-4 w-full group-[]: ">
         <h1 className="text-gray-900 text-[1.20rem] capitalize group-hover:bg-opacity-85 p-2.5 border-b-[1px] border-gray-400">
           {label}
@@ -31,87 +40,75 @@ const ItemCard = ({
               src={imgUrl}
               alt={label}
             />
-            <button
-              type="button"
-              className="flex items-center gap-2 text-sm px-5 rounded-xl text-wikimedia-wikiblue hover:text-wikimedia-darkblue border border-wikimedia-wikiblue hover:font-medium hover:opacity-85 transition-colors"
-              aria-haspopup="dialog"
-              aria-expanded="false"
-              aria-controls="hs-vertically-centered-modal"
-              data-hs-overlay="#hs-vertically-centered-modal"
-            >
-              <span>Edit</span>
-              <PencilIcon className="w-3.5 h-3.5" />
-            </button>
+            <div className="w-fit">
+              <Button
+                label="Edit"
+                border={true}
+                onClick={() => setShowModal(!showModal)}
+              />
+            </div>
           </div>
-          {/* see more button */}
-          <button
-            type="button"
-            className="hs-collapse-toggle w-24 flex items-center justify-center px-2 gap-2 text-base rounded-xl text-white font-medium bg-wikimedia-wikiblue border-wikimedia-wikiblue hover:font-medium hover:opacity-85 transition-colors"
-            id={`${collapseId}-toggle`}
-            data-hs-collapse={`#${collapseId}`}
-          >
-            <span className="hs-collapse-open:hidden">See More</span>
-            <span className="hs-collapse-open:block hidden">See Less</span>
-          </button>
+          <div className="w-fit">
+            <Button label={btnText} onClick={handleMoreContent} />
+          </div>
         </div>
 
         {/* collapse content (more item props) */}
-        <div
-          id={collapseId}
-          className="hs-collapse hidden w-full overflow-hidden transition-[height] duration-300"
-          aria-labelledby={`${collapseId}-toggle`}
-        >
-          <p>Total Files: {totalFiles}</p>
-          <p>Founded Date: {foundedDate}</p>
-        </div>
+        {seeMore && (
+          <div>
+            <p>Total Files: {totalFiles}</p>
+            <p>Founded Date: {foundedDate}</p>
+          </div>
+        )}
 
         {/* modal content */}
-        <div
-          id="hs-vertically-centered-modal"
-          className="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
-          role="dialog"
-          tabIndex={-1}
-          aria-labelledby="hs-vertically-centered-modal-label"
-        >
-          <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)] flex items-center">
-            <div className="w-full flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-neutral-800 dark:border-neutral-700 dark:shadow-neutral-700/70">
-              <div className="flex justify-between items-center py-3 px-4 border-b dark:border-neutral-700">
-                <h3
-                  id="hs-vertically-centered-modal-label"
-                  className="font-bold text-gray-800 dark:text-white"
-                >
-                  Edit Item
-                </h3>
-              </div>
-              {/* content */}
-              <div className="p-4 overflow-y-auto">
-                 {/* dropdown-menu */}
+        {showModal && (
+          <div
+            id="hs-vertically-centered-modal"
+            className="hs-overlay hidden size-full fixed top-0 start-0 z-[80] overflow-x-hidden overflow-y-auto pointer-events-none"
+            role="dialog"
+            tabIndex={-1}
+            aria-labelledby="hs-vertically-centered-modal-label"
+          >
+            <div className="hs-overlay-open:mt-7 hs-overlay-open:opacity-100 hs-overlay-open:duration-500 mt-0 opacity-0 ease-out transition-all sm:max-w-lg sm:w-full m-3 sm:mx-auto min-h-[calc(100%-3.5rem)] flex items-center">
+              <div className="w-full flex flex-col bg-white border shadow-sm rounded-xl pointer-events-auto dark:bg-neutral-800 dark:border-neutral-700 dark:shadow-neutral-700/70">
+                <div className="flex justify-between items-center py-3 px-4 border-b dark:border-neutral-700">
+                  <h3
+                    id="hs-vertically-centered-modal-label"
+                    className="font-bold text-gray-800 dark:text-white"
+                  >
+                    Edit Item
+                  </h3>
+                </div>
+                {/* content */}
+                <div className="p-4 overflow-y-auto">
+                  {/* dropdown-menu */}
 
-                 {/* search input */}
-                 <EditSearch />
-              </div>
-              <div className="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-neutral-700">
-                <button
-                  type="button"
-                  className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
-                  data-hs-overlay="#hs-vertically-centered-modal"
-                >
-                  Close
-                </button>
-                <button
-                  type="button"
-                  className="py-1.5 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-wikimedia-wikiblue text-white hover:bg-wikimedia-darkblue focus:outline-none focus:bg-wikimedia-wikiblue disabled:opacity-50 disabled:pointer-events-none"
-                >
-                  Save changes
-                </button>
+                  {/* search input */}
+                  <EditSearch />
+                </div>
+                <div className="flex justify-end items-center gap-x-2 py-3 px-4 border-t dark:border-neutral-700">
+                  <button
+                    type="button"
+                    className="py-2 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-gray-200 bg-white text-gray-800 shadow-sm hover:bg-gray-50 focus:outline-none focus:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 dark:border-neutral-700 dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                    data-hs-overlay="#hs-vertically-centered-modal"
+                  >
+                    Close
+                  </button>
+                  <button
+                    type="button"
+                    className="py-1.5 px-3 inline-flex items-center gap-x-2 text-sm font-medium rounded-lg border border-transparent bg-wikimedia-wikiblue text-white hover:bg-wikimedia-darkblue focus:outline-none focus:bg-wikimedia-wikiblue disabled:opacity-50 disabled:pointer-events-none"
+                  >
+                    Save changes
+                  </button>
+                </div>
               </div>
             </div>
           </div>
-        </div>
+        )}
       </div>
     </div>
   );
 };
 
 export default ItemCard;
-
--- src/components/button.tsx
+++ src/components/button.tsx
@@ -1,18 +1,26 @@
+// import { PencilIcon } from "lucide-react";
+import { PencilIcon } from "@heroicons/react/24/outline";
+
 import React from "react";
 
 interface ButtonProps {
   label: string;
   onClick: () => void;
+  border?: boolean
 }
 
-const Button: React.FC<ButtonProps> = ({ label, onClick }) => {  
-  return (  
-    <button  
-      className="px-3 py-2 bg-wikimedia-wikiblue text-white rounded-md hover:bg-wikimedia-lightblue focus:outline-none focus:ring-2 focus:ring-blue-500"  
-      onClick={onClick}  
-    >  
-      {label}  
-    </button>  
+const Button: React.FC<ButtonProps> = ({ label, border, onClick }) => {  
+  const bgColor = border ? "white" : "wikimedia-wikiblue";
+  const textColor = border ? "wikimedia-wikiblue" : "white";
+
+  return (
+    <button
+      className={`px-4 py-2 flex items-center gap-2 border  border-wikimedia-wikiblue bg-${bgColor} text-${textColor} rounded-sm`}
+      onClick={onClick}
+    >
+      {label}
+      {border && <PencilIcon className="w-3.5 h-3.5" />}
+    </button>
   );  
 };  
 
