45ae0606f045fc3155eef14855ef5049e99f46d6
onoyima <clintonfaze@outlook.com>
2024-11-18 16:47:04 +0100

    Setup Material UI for the project (T379998)

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,12 +1,17 @@
 import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
+import MUIDemo from "./components/MUIDemo";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-  }
+  },
+  {
+    path: "/mui-demo",
+    element: <MUIDemo />,
+  },
 ]);
 
 export default router;
